76a6bfbfcb2de5776c99aa1dba3bd20e974111b9
austin-do <43225674+austin-do@users.noreply.github.com>
2018-09-14 15:59:46 -0400

    class: added PlayerHand class

---
+++ src/main/java/core/PlayerHand.java
@@ -0,0 +1,12 @@
+package core;
+
+public class PlayerHand {
+	private String[] cards;
+	
+	public PlayerHand(String card1, String card2, String card3, String card4) {
+		this.cards[0] = card1;
+		this.cards[1] = card2;
+		this.cards[2] = card3;
+		this.cards[3] = card4;
+	}
+}
