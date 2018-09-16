76c52e9a07788e6a650af0287e49db3a91196b45
austin-do <43225674+austin-do@users.noreply.github.com>
2018-09-16 11:13:07 -0400

    test: ConsoleInputTest changes

--- src/test/java/core/ConsoleInputTest.java
+++ src/test/java/core/ConsoleInputTest.java
@@ -9,12 +9,10 @@ import org.junit.Test;
 public class ConsoleInputTest {
 
 	@Test
-	public void test() {
-		Scanner sc = new Scanner(System.in);
-		String cinput = sc.nextLine();
-		sc.close();
+	public void testConsoleInput() {
 		
-		assertEquals("c", cinput);
+		assertTrue(ConsoleInput());
+		 
 	}
 
 }
