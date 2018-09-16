004c5d3bde8551f04148de14ece6d845288c537b
austin-do <43225674+austin-do@users.noreply.github.com>
2018-09-16 11:17:34 -0400

    class: ConsoleInput added. Minor changes to ConsoleInputTest

---
+++ src/main/java/core/ConsoleInput.java
@@ -0,0 +1,7 @@
+package core;
+
+public class ConsoleInput {
+	public boolean runGame() {
+		return true;
+	}
+}
--- src/test/java/core/ConsoleInputTest.java
+++ src/test/java/core/ConsoleInputTest.java
@@ -2,17 +2,14 @@ package core;
 
 import static org.junit.Assert.*;
 
-import java.util.Scanner;
-
 import org.junit.Test;
 
 public class ConsoleInputTest {
 
 	@Test
 	public void testConsoleInput() {
-		
-		assertTrue(ConsoleInput());
-		 
+		ConsoleInput cinput = new ConsoleInput();
+		assertTrue(cinput.runGame());
 	}
 
 }
