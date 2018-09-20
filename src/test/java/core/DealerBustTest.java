f8b64c76c14595937997b72008cdcc8180c34404
austin-do <43225674+austin-do@users.noreply.github.com>
2018-09-19 21:59:16 -0400

    test: added PlayerBlackJackTest testcase.

--- src/test/java/core/DealerBustTest.java
+++ src/test/java/core/DealerBustTest.java
@@ -8,7 +8,15 @@ public class DealerBustTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		PlayerHand hand = new PlayerHand( new Card("H10"), new Card("HJ"));
+		
+		hand.addCard(new Card("HQ"));
+		
+		assertTrue(hand.Value() > 21);
+		
+		PlayerHand hand2 = new PlayerHand( new Card("H10"), new Card("HJ"));
+		
+		assertTrue(hand2.Value() < 21);
 	}
 
 }
---
+++ src/test/java/core/PlayerBlackJackTest.java
@@ -0,0 +1,22 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerBlackJackTest {
+
+	@Test
+	public void test() {
+		
+		ConsoleInput cinput = new ConsoleInput();
+		
+		PlayerHand player = new PlayerHand( new  Card("H10"), new Card("HA"));
+	
+		cinput.PlayerTurn(player);
+		
+		assertTrue(player.hasBlackJack());
+
+	}
+
+}
