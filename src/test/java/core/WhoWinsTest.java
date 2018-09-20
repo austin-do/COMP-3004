df1089adac2fa354ead8833c41863e35a9ae0f23
austin-do <43225674+austin-do@users.noreply.github.com>
2018-09-19 23:09:58 -0400

    test: updated WhoWinsTest testcase for R40

--- src/test/java/core/WhoWinsTest.java
+++ src/test/java/core/WhoWinsTest.java
@@ -9,6 +9,8 @@ public class WhoWinsTest {
 	@Test
 	public void test() {
 		ConsoleInput cinput = new ConsoleInput();
+		
+		//If player has BlackJack but dealer doesn't
 		PlayerHand player = new PlayerHand( new  Card("H10"), new Card("HA"));
 		PlayerHand dealer = new PlayerHand( new  Card("H10"), new Card("H8"));
 		
@@ -17,8 +19,14 @@ public class WhoWinsTest {
 		
 		//if winner is a player
 		assertEquals(1, cinput.winner);
-
-
+		
+		//If dealer has BlackJack 
+		PlayerHand dealer2 = new PlayerHand( new  Card("H10"), new Card("H8"));
+		
+		cinput.BlackJack(player);
+		cinput.BlackJack(dealer);
+		
+		cinput.WhoWins(player, dealer2);
 		
 	}
 
