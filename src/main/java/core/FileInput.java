36491d6e1f48473555efe47d8be1ac65a84a89bb
austin-do <43225674+austin-do@users.noreply.github.com>
2018-09-19 23:37:36 -0400

    test: added PlayerScoreTest testcase.

--- src/main/java/core/FileInput.java
+++ src/main/java/core/FileInput.java
@@ -272,4 +272,5 @@ public class FileInput {
 			this.winner = 2;
 		}
 	}
+	
 }
---
+++ src/test/java/core/PlayerScoreTest.java
@@ -0,0 +1,18 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerScoreTest {
+
+	@Test
+	public void test() {
+		
+		ConsoleInput cinput = new ConsoleInput();
+		PlayerHand player = new PlayerHand(new Card("H10"), new Card("H8"));
+		int val = cinput.PrintPlayerValue(player);
+		assertEquals(18, val);
+	}
+
+}
